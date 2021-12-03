16a4b3343a5a6e130745631e049513a697b25b31
Aleksandar Milosevic <aleksandar_mlsvc@hotmail.com>
2021-12-03 22:50:23 +0100

    Updated read model for hospital treatment

--- src/services/HospitalTreatmentsReadService.ts
+++ src/services/HospitalTreatmentsReadService.ts
@@ -1,11 +1,52 @@
-import { EventStoreDBClient, START, streamNameFilter } from "@eventstore/db-client";
-import { RedisClientType } from "@node-redis/client/dist/lib/client";
+import {
+    HospitalTreatmentCreatedEvent,
+    TherapyAddedToHospitalTreatmentEvent,
+} from '@app/hospitalTreatment/persistance/HospitalTreatmentEventStore';
+import { EventStoreDBClient, START, streamNameFilter } from '@eventstore/db-client';
+import { RedisClientType } from '@node-redis/client/dist/lib/client';
 
-interface ChangeObject<T> {
-    (obj: T): Promise<T>
+class HospitalTreatment {
+    public treatmentId: string;
+    public medicalCardId: string;
+    public doctorId: string;
+    public therapies: string[];
+
+    constructor(data: {
+        treatmentId: string,
+        medicalCardId: string,
+        doctorId: string,
+        therapies: string[]
+    }) {
+        this.treatmentId = data.treatmentId;
+        this.medicalCardId = data.medicalCardId;
+        this.doctorId = data.doctorId;
+        this.therapies = data.therapies;
+    }
+
+}
+interface ChangeObject {
+    (obj: HospitalTreatment): Promise<HospitalTreatment>
+}
+interface Handler {
+    (redis: RedisClientType<any>, event: any): Promise<void>
 }
 
 export default class HospitalTreatmentsReadService {
+
+    private readonly _handlers: Map<string, Handler> = new Map();
+
+    constructor() {
+        this._handlers
+            .set("hospital-treatment-created", async (redis, { data }: HospitalTreatmentCreatedEvent) => {
+                await redis.hSet("hospital-treatments", data.treatmentId, JSON.stringify(new HospitalTreatment({ ...data, therapies: [] })))
+            })
+            .set("therapy-added-to-treatment", async (redis, { data }: TherapyAddedToHospitalTreatmentEvent) => {
+                await this.changeTreatment(redis, data.treatmentId, async (treatment) => {
+                    treatment.therapies.push(data.therapyId)
+                    return treatment;
+                })
+            })
+    }
 
     async read(client: EventStoreDBClient, redis: RedisClientType<any>): Promise<void> {
         const subscription = client.subscribeToAll({
@@ -15,23 +56,15 @@ export default class HospitalTreatmentsReadService {
         })
         for await (const { event } of subscription) {
             if (event === undefined) continue;
-            if (event.type === "hospital-treatment-created") {
-                await redis.hSet("hospital-treatments", (event as any).data.treatmentId, JSON.stringify({ ...event.data as any, therapies: [] }))
-            }
-
-            if (event.type === "therapy-added-to-treatment") {
-                await this.changeTreatment(redis, (event as any).data.treatmentId, async (treatment) => {
-                    treatment.therapies.push((event as any).data.therapyId)
-                    return treatment;
-                })
-            }
+            const handler = this._handlers.get(event.type)
+            if (handler === undefined) continue;
+            await handler(redis, event)
         }
     }
 
-    private async changeTreatment(redis: RedisClientType<any>, id: string, change: ChangeObject<any>) {
+    private async changeTreatment(redis: RedisClientType<any>, id: string, change: ChangeObject) {
         let dataString = await redis.hGet("hospital-treatments", id)
         if (dataString === undefined) return
-        await redis.hSet("hospital-treatments", id, JSON.stringify(await change(JSON.parse(dataString))))
+        await redis.hSet("hospital-treatments", id, JSON.stringify(await change(JSON.parse(dataString) as HospitalTreatment)))
     }
-
 }
