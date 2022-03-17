eecde05255b8124fabf78a7463e6631e7873352d
Aleksandar Milosevic <aleksandar_mlsvc@hotmail.com>
2022-03-17 20:20:06 +0100

    Db update  #2

--- migrations/medication_05_therapy_medicaments.ts
+++ migrations/medication_05_therapy_medicaments.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.withSchema("medication").createTable("therapy_medicaments", (table) => {
         table.string("therapy").notNullable().index().references("id").inTable("medication.therapy").onUpdate("CASCADE").onDelete("CASCADE");
-        table.string("medicament_id").notNullable();
+        table.string("medicament_id").notNullable().index().references("id").inTable("medication.medicaments").onUpdate("CASCADE").onDelete("CASCADE");
         table.integer("strength").notNullable();
         table.integer("amount").notNullable();
         table.string("route").notNullable();
